7a7bec8b9186b77dfd587edfd377be95ac2e073a
jerome <ambroise.jerome.formation@gmail.com>
2018-06-12 13:07:31 +0200

    Controleur de Symfony : refactoring HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -12,58 +13,27 @@ use Twig\Environment;
  * Class HomeController
  * Classe de contrôleur pour tester les routes
  */
-class HomeController
+class HomeController extends Controller
 {
-
-    /**
-     * On stocke le générateur d'URL pour pouvoir l'utiliser dans les méthodes de la classe
-     * @var UrlGeneratorInterface
-     */
-    private $urlGeneratorReceived;
-
-    /**
-     * On créé un constructeur pour pouvoir "injecter" une classe qui nous permettra de créer un lien
-     * Cette classe devra implémenter "UrlGeneratorInterface"
-     * Le principe de l'injection de dépendances :
-     *      - On demande une classe (ou une interface)
-     *      - "Symfony" (son container d'injection de dépendances) nous retourne,
-     *        un objet instancié de cette classe
-     *
-     * @param UrlGeneratorInterface $generator
-     */
-    public function __construct(UrlGeneratorInterface $generator)
-    {
-        // On attribue à notre propriété l'objet donné par "Symfony"
-        $this->urlGeneratorReceived = $generator;
-    }
 
     /**
      * On souhaite générer dans la page d'index, un lien vers le blog
      * On utilise donc la propriété qui stocke le générateur d'URL
      *
-     * @param Environment $twig
      * @return Response
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
      */
-    public function index(Environment $twig): Response
+    public function index(): Response
     {
         // On génère l'URL
-        $path = $this->urlGeneratorReceived->generate('app_home_blog', [
+        $path = $this->generateUrl('app_home_blog', [
             "nombre" => 64,
             "theme" => 'detente'
         ]);
 
         // On créé la réponse (en passant le lien en paramètre)
-        $response = new Response(
-            $twig->render('index.html.twig', [
-                'lien' => $path
-            ])
-        );
-
-        // On renvoie la réponse au navigateur
-        return $response;
+        return $this->render('index.html.twig', [
+            'lien' => $path
+        ]);
     }
 
 
@@ -72,21 +42,15 @@ class HomeController
      *
      * @Route("/about")
      *
-     * @param Environment $twig
      * @return Response
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
      */
-    public function about(Environment $twig): Response
+    public function about(): Response
     {
         // On retourne une réponse au navigateur
         // Cette réponse appelle une vue en lui passant un paramètre
-        return new Response(
-            $twig->render('presentation/about.html.twig', [
-                'prenom' => 'Billy'
-            ])
-        );
+        return $this->render('presentation/about.html.twig', [
+            'prenom' => 'Billy'
+        ]);
     }
 
     /**
@@ -109,23 +73,17 @@ class HomeController
      *     "theme" = "[a-z]+"
      * })
      *
-     * @param Environment $twig
      * @param int $nombre Variable obligatoire de l'URL
      * @param string $theme Variable facultative de l'URL (valeur par défaut)
      * @return Response
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
      */
-    public function blog(Environment $twig, int $nombre, string $theme = 'Détente'): Response
+    public function blog(int $nombre, string $theme = 'Détente'): Response
     {
-        return new Response(
-            $twig->render('blog/index.html.twig', [
-                'title'  => 'Bienvenue sur le blog',
-                'nbPage' => $nombre,
-                'theme'    => $theme
-            ])
-        );
+        return $this->render('blog/index.html.twig', [
+            'title'  => 'Bienvenue sur le blog',
+            'nbPage' => $nombre,
+            'theme'    => $theme
+        ]);
     }
 }
 
