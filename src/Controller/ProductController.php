12dfd9aeab6daefbd3071aadfbf1b12b20e261ae
jerome <ambroise.jerome.formation@gmail.com>
2018-06-14 14:16:34 +0200

    Mise en place de la BDD - Creation entité product - Ajout et Liste

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,41 +19,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends Controller
 {
     /**
-     * Tableau statique de produits
-     * @var array
-     */
-    private $products = [
-        [
-            "id"            => 1,
-            "name"          => 'hamac',
-            "description"   => 'Pour se détendre lorsqu\'il arrêtera de pleuvoir'
-        ],
-        [
-            "id"            => 2,
-            "name"          => 'parasol',
-            "description"   => 'Pour faire de l\'ombre au hamac'
-        ],
-        [
-        "id"                => 3,
-            "name"          => 'Le sable',
-            "description"   => 'Pour tenir le parasol'
-        ]
-    ];
-
-    /**
      * Liste les différents produits
      * @Route("/produits")
      * @return Response
      */
     public function index(): Response
     {
-        // Récupération des produits
-        $products = $this->products;
+        /* Récupération des produits */
+        // On récupère le Repository
+        $repository = $this->getDoctrine()
+            ->getRepository(Product::class);
+        // On récupère les produits
+        $products = $repository->findAll();
 
-        // Renvoi des produits à la vue
+        // Renvoi les produits à la vue
         return $this->render('products/index.html.twig', [
                 'products' => $products
             ]
         );
+    }
+
+    /**
+     * Ajoute un produit en BDD
+     * @Route("/produits/ajout")
+     * @return Response
+     */
+    public function add(): Response
+    {
+        // Création d'une instance de notre entité
+        $product = new Product();
+
+        // Attribution de données à l'entité
+        $product->setName('Parasol');
+        $product->setDescription('Pour faire de l\'ombre au hamac');
+        $product->setPrice(155.99);
+
+        // Récupération du manager de doctrine
+        $manager = $this->getDoctrine()->getManager();
+        // Enregistrement du produit en BDD
+        $manager->persist($product);
+        $manager->flush();
+
+        return $this->redirectToRoute('app_product_index');
     }
 }
